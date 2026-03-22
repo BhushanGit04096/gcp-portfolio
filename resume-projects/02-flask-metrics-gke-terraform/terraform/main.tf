e3889eb33d3605d25fdc1869b2ad1d298d0e45b8
Naga Bhushanam Meka <nagabhushancherry@gmail.com>
2026-03-22 19:33:19 +0530

    added subnet and firewall rules

--- resume-projects/02-flask-metrics-gke-terraform/terraform/main.tf
+++ resume-projects/02-flask-metrics-gke-terraform/terraform/main.tf
@@ -4,8 +4,29 @@ provider "google" {
   
 }
 
-resource "google_compute_network" "name" {
+resource "google_compute_network" "vpc" {
     name = "flask-metrics-vpc"
     auto_create_subnetworks = false
+  
+}
+
+resource "google_compute_subnetwork" "subnet" {
+    name = "flask-metrics-subnet"
+    ip_cidr_range = "10.0.0.0/24"
+    region = var.region
+    network = google_compute_network.vpc.id
+  
+}
+
+resource "google_compute_firewall" "name" {
+    name = "flask-metrics-allow-web"
+    network = google_compute_network.vpc.id
+
+    allow {
+      protocol = "tcp"
+      ports = [ "80" , "443" , "5000" ]
+    }
+
+    source_ranges = "0.0.0.0/0"
   
 }
