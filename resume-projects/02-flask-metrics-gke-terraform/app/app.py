fc66b2d0142b75229053d03debf61c22737f4e67
Naga Bhushanam Meka <nagabhushancherry@gmail.com>
2026-03-22 08:54:23 +0530

    added py and dockerfiles

---
+++ resume-projects/02-flask-metrics-gke-terraform/app/app.py
@@ -0,0 +1,20 @@
+from flask import Flask, jsonify
+import psutil
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Flask Metrics App is running!"
+
+@app.route('/metrics')
+def metrics():
+    return jsonify({
+        "cpu_percent": psutil.cpu_percent(interval=1),
+        "memory_percent": psutil.virtual_memory().percent,
+        "memory_used_mb": round(psutil.virtual_memory().used / 1024 / 1024, 2),
+        "memory_total_mb": round(psutil.virtual_memory().total / 1024 / 1024, 2)
+    })
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
